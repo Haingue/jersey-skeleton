0514b4b2b8bc9517fa2900a912cafd231d07a5c3
Valentin VERSPEEK <valentin.verspeek@etudiant.univ-lille1.fr>
2018-03-21 15:50:28 +0100

    ajout recherche par ID d'une Corp pour CorpDAO

--- server/src/main/java/fr/iutinfo/rest/CorpDAO.java
+++ server/src/main/java/fr/iutinfo/rest/CorpDAO.java
@@ -1,9 +1,38 @@
 package fr.iutinfo.rest;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CorpDAO {
-	
+
 	public Corp getCorpById(int id) {
-		Connection con = 
+		Corp corp = null;
+		Connection con = new BDDFactory().getConnection();
+		try {
+			PreparedStatement ps = con.prepareStatement("SELECT * FROM corp WHERE cno=?");
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				String name = rs.getString("name");
+				String domain = rs.getString("domain");
+				corp = new Corp(id, name, domain);
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+			}
+		}
+
+		return corp;
 	}
 
 }
