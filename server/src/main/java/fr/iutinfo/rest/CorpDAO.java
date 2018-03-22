d71193b90198e32d6bc9f82647a1cfd160fedc18
Valentin VERSPEEK <valentin.verspeek@etudiant.univ-lille1.fr>
2018-03-22 10:05:37 +0100

    ajout méthode initialisation table Corporate

--- server/src/main/java/fr/iutinfo/rest/CorpDAO.java
+++ server/src/main/java/fr/iutinfo/rest/CorpDAO.java
@@ -2,8 +2,17 @@ package fr.iutinfo.rest;
 
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
 public interface CorpDAO {
+	
+	@SqlUpdate("create table corporate("
+			+ "cno integer primary key autoincrement,"
+			+ "name varchar(50) not null,"
+			+ "domain varchar(50) not null,"
+			+ "constraint uniq_corporate_name unique (name),"
+			+ "constraint uniq_domain unique (domain)")
+	public void createCorpTable();
 	
 	@SqlQuery("select * from corporate where cno = :id")
 	public Corp getById(@Bind("id") int id);
