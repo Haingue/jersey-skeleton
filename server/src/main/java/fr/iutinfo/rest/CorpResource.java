718c4fb740b9b8a8a08b002a95caf88df5351890
Juliette PARISSEAUX <juliette.parisseaux@etudiant.univ-lille1.fr>
2018-03-22 11:16:36 +0100

    Added CorpResource.java

---
+++ server/src/main/java/fr/iutinfo/rest/CorpResource.java
@@ -0,0 +1,45 @@
+package fr.iutinfo.rest;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("/corp")
+public class CorpResource {
+	
+    private static Map<Integer, Corp> corps = new HashMap<>();
+    
+    final static Logger logger = LoggerFactory.getLogger(TempoResource.class);
+	
+    @Context
+    public UriInfo uriInfo;
+
+    public CorpResource() {}
+    
+    @POST
+    public Response createCorp(Corp corp) {
+    	// Si le corp existe déjà, renvoyer 409
+        if ( corps.containsKey(corp.getCno()) ) {
+            return Response.status(Response.Status.CONFLICT).build();
+        }
+        else {
+        	corps.put(corp.getCno(), corp);
+
+            // On renvoie 201 et l'instance de la ressource dans le Header HTTP 'Location'
+            URI instanceURI = uriInfo.getAbsolutePathBuilder().path( String.valueOf(corp.getCno()) ).build();
+            return Response.created(instanceURI).build();
+        }
+    	
+    }
+}
