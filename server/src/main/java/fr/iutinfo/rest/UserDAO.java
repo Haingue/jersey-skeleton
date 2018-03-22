2fc268e14a795fd4e4586773ba9343baf0c4b393
Fabien HAINGUE <fabien.haingue@etudiant.univ-lille1.fr>
2018-03-22 10:11:02 +0100

    add creatUserTable

--- server/src/main/java/fr/iutinfo/rest/UserDAO.java
+++ server/src/main/java/fr/iutinfo/rest/UserDAO.java
@@ -11,46 +11,42 @@ import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
 import org.skife.jdbi.v2.tweak.BeanMapperFactory;
 
 public interface UserDAO {
-    /*@SqlUpdate("CREATE TABLE users\n" + 
-    		"(\n" + 
-    		"	uno SERIAL PRIMARY KEY,\n" + 
-    		"	login VARCHAR(100) NOT NULL,\n" + 
-    		"	password VARCHAR(50) NOT NULL,\n" + 
-    		"	nom VARCHAR(50) NOT NULL,\n" + 
-    		"	prenom VARCHAR(50) NOT NULL,\n" + 
-    		"	fonction VARCHAR(100),\n" + 
-    		"	cno INT NOT NULL,\n" + 
-    		"\n" + 
-    		"	CONSTRAINT fk_cno FOREIGN KEY (cno)\n" + 
-    		"		REFERENCES corporate(cno),\n" + 
-    		"\n" + 
-    		"	CONSTRAINT uniq_login UNIQUE (login)\n" + 
-    		")")
-    void createUserTable();
-    
-    @SqlUpdate("drop table if exists users")
-    void dropUserTable();    
-    */
-	
-    @SqlUpdate("insert into users (login, password, nom, prenom, fonction, cno) values (:login, :password, :nom, :prenom, :fonction, :cno)")
-    @GetGeneratedKeys
-    int insert(@BindBean() User user);
-    
-    @SqlQuery("select * from users where login = :login")
-    @RegisterMapperFactory(BeanMapperFactory.class)
-    User findByLogin(@Bind("login") String login);
-    
-    @SqlQuery("select * from users where login = :login and password = :password")
-    @RegisterMapperFactory(BeanMapperFactory.class)
-    User checkUser(@Bind("login") String login, @Bind("password") String password);
-    
-    @SqlQuery("select * from users order by uno")
-    @RegisterMapperFactory(BeanMapperFactory.class)
-    List<User> all();
-    
-    @SqlQuery("select * from users where uno = :uno")
-    @RegisterMapperFactory(BeanMapperFactory.class)
-    User findById(@Bind("uno") int uno);
-    
-    void close();
+	/*
+	 * @SqlUpdate("CREATE TABLE users\n" + "(\n" + "	uno SERIAL PRIMARY KEY,\n" +
+	 * "	login VARCHAR(100) NOT NULL,\n" + "	password VARCHAR(50) NOT NULL,\n" +
+	 * "	nom VARCHAR(50) NOT NULL,\n" + "	prenom VARCHAR(50) NOT NULL,\n" +
+	 * "	fonction VARCHAR(100),\n" + "	cno INT NOT NULL,\n" + "\n" +
+	 * "	CONSTRAINT fk_cno FOREIGN KEY (cno)\n" +
+	 * "		REFERENCES corporate(cno),\n" + "\n" +
+	 * "	CONSTRAINT uniq_login UNIQUE (login)\n" + ")") void createUserTable();
+	 * 
+	 * @SqlUpdate("drop table if exists users") void dropUserTable();
+	 */
+
+	@SqlUpdate("create table users (" + "cno integer primary key autoincrement,"
+			+ " login varchar(200), password varchar(60)," + " nom varchar(100), prenom varchar(100)," + " cno integer,"
+			+ " CONSTRAINT fk_cno FOREIGN KEY (cno)" + "REFERENCES corporate(cno) )")
+	void createUserTable();
+
+	@SqlUpdate("insert into users (login, password, nom, prenom, fonction, cno) values (:login, :password, :nom, :prenom, :fonction, :cno)")
+	@GetGeneratedKeys
+	int insert(@BindBean() User user);
+
+	@SqlQuery("select * from users where login = :login")
+	@RegisterMapperFactory(BeanMapperFactory.class)
+	User findByLogin(@Bind("login") String login);
+
+	@SqlQuery("select * from users where login = :login and password = :password")
+	@RegisterMapperFactory(BeanMapperFactory.class)
+	User checkUser(@Bind("login") String login, @Bind("password") String password);
+
+	@SqlQuery("select * from users order by uno")
+	@RegisterMapperFactory(BeanMapperFactory.class)
+	List<User> all();
+
+	@SqlQuery("select * from users where uno = :uno")
+	@RegisterMapperFactory(BeanMapperFactory.class)
+	User findById(@Bind("uno") int uno);
+
+	void close();
 }
